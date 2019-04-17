e87087ae03914cd902d81e1a7ed462152962dcdc
Marcel Schneider <marcel.sch@web.de>
2019-04-17 22:07:35 +0200

    separated backend and frontend

---
+++ anstoss-transfer-market-frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  baseUrl: 'https://mmagic.alnilam.uberspace.de/anstoss/backend'
+};
--- anstoss-transfer-market/src/main/resources/frontend/anstoss-transfer-market/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  baseUrl: 'http://mmagic.alnilam.uberspace.de:62763'
-};
