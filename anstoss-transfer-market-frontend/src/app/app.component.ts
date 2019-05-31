fcb582d748f0e92b8d9d53dd4fcd60c79d423039
Marcel Schneider <marcel.sch@web.de>
2019-05-31 18:05:57 +0200

    mat table prototype

--- anstoss-transfer-market-frontend/src/app/app.component.ts
+++ anstoss-transfer-market-frontend/src/app/app.component.ts
@@ -1,58 +1,39 @@
-import {Component} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {AfterViewInit, Component, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {Player} from "./Player";
-import {Sort} from "@angular/material";
+import {MatSort, MatTableDataSource, Sort} from "@angular/material";
 import {environment} from "../environments/environment";
+import {throwError} from "rxjs";
+import {catchError} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
 
-  players: Player[] = [];
-  sortedData: Player[] = [];
+  errorMessage: string = null;
+  displayedColumns: string[] = ['position', 'name', 'strength', 'age', 'country', 'price'];
+  dataSource = new MatTableDataSource<Player>();
+
+  @ViewChild(MatSort)
+  set sort(v: MatSort) {
+    this.dataSource.sort = v;
+  }
 
   constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
     this.http.get<Player[]>(environment.baseUrl + "/search").subscribe(
-      players => {
-        this.players = players;
-        this.sortedData = this.players.slice();
-      }
+      players => this.dataSource.data = players,
+      () => this.errorMessage = "Backend not reachable"
     );
   }
 
-  sortData(sort: Sort) {
-    const data = this.players.slice();
-    if (!sort.active || sort.direction === '') {
-      this.sortedData = data;
-      return;
-    }
-
-    this.sortedData = data.sort((a, b) => {
-      const isAsc = sort.direction === 'asc';
-      switch (sort.active) {
-        case 'name':
-          return compare(a.name, b.name, isAsc);
-        case 'age':
-          return compare(a.age, b.age, isAsc);
-        case 'strength':
-          return compare(a.strength, b.strength, isAsc);
-        case 'position':
-          return compare(a.position, b.position, isAsc);
-        case 'country':
-          return compare(a.country, b.country, isAsc);
-        case 'price':
-          return compare(a.price, b.price, isAsc);
-        default:
-          return 0;
-      }
-    });
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
   }
-}
-
-function compare(a: number | string, b: number | string, isAsc: boolean) {
-  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
 
