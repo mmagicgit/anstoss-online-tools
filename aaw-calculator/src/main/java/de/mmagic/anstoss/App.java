f9aacd3d51dc36d54b9c57f7298b5040acdb65a5
Marcel Schneider <marcel.sch@web.de>
2021-07-10 17:23:38 +0200

    addition of missing positions

--- aaw-calculator/src/main/java/de/mmagic/anstoss/App.java
+++ aaw-calculator/src/main/java/de/mmagic/anstoss/App.java
@@ -6,21 +6,27 @@ import com.google.common.collect.Range;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class App {
 
     public static void main(String[] args) throws IOException {
-        String path2 = "/home/ms/Entwicklung/Git/anstoss-online-tools/aaw-calculator/src/main/resources/2018_1.html";
-        String path3 = "/home/ms/Entwicklung/Git/anstoss-online-tools/aaw-calculator/src/main/resources/2018_2.html";
-        String path4 = "/home/ms/Entwicklung/Git/anstoss-online-tools/aaw-calculator/src/main/resources/2019_1.html";
-        String path5 = "/home/ms/Entwicklung/Git/anstoss-online-tools/aaw-calculator/src/main/resources/2019_2.html";
-        String path6 = "/home/ms/Entwicklung/Git/anstoss-online-tools/aaw-calculator/src/main/resources/2020_1.html";
-
+        Stream<String> files = Stream.of(
+                "/home/ms/Entwicklung/Git/anstoss-online-tools/aaw-calculator/src/main/resources/2018_1.html",
+                "/home/ms/Entwicklung/Git/anstoss-online-tools/aaw-calculator/src/main/resources/2018_2.html",
+                "/home/ms/Entwicklung/Git/anstoss-online-tools/aaw-calculator/src/main/resources/2019_1.html",
+                "/home/ms/Entwicklung/Git/anstoss-online-tools/aaw-calculator/src/main/resources/2019_2.html",
+                "/home/ms/Entwicklung/Git/anstoss-online-tools/aaw-calculator/src/main/resources/2020_1.html",
+                "/home/ms/Entwicklung/Git/anstoss-online-tools/aaw-calculator/src/main/resources/2020_2.html",
+                "/home/ms/Entwicklung/Git/anstoss-online-tools/aaw-calculator/src/main/resources/2021_1.html",
+                "/home/ms/Entwicklung/Git/anstoss-online-tools/aaw-calculator/src/main/resources/2021_2.html"
+        );
         Ordering<String> ordering = Ordering.explicit(
                 "Schnelligkeit", "Zweikampf", "Kopfball", "Schusskraft", "Schussgenauigkeit", "Technik", "Spielintelligenz",
                 "Reflexe", "Paraden", "Lufthoheit", "Herauslaufen", "Ballsicherheit");
 
-        new AawParser().readFiles(path2, path3, path4, path5, path6).forEach(player -> {
+        new AawParser().readFiles(files.collect(Collectors.toList())).forEach(player -> {
             System.out.println("\n" + player.name + " (" + player.data.size() / player.data.keySet().size() + " AAWs)");
             ordering.sortedCopy(player.data.keySet()).forEach(key -> {
                 List<Aaw> aaws = player.data.get(key);
