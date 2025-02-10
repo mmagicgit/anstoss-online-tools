a9dddf2a4458d0f36640c39ef0f3a3019cf413c0
Marcel Schneider <marcel.schneider@bridging-it.de>
2025-02-10 11:35:02 +0100

    form selection state

--- anstoss-transfer-market-react/components/SearchForm.tsx
+++ anstoss-transfer-market-react/components/SearchForm.tsx
@@ -1,4 +1,37 @@
+import {useState} from "react";
+import {FormSelection} from "@/model/FormSelection";
+
+const initialState = {
+    ageFrom: 18,
+    ageTo: 30,
+    strengthFrom: 4,
+    strengthTo: 11,
+    position: ["MD", "RV", "LV", "LIB", "LM", "RM", "ZM", "ST"],
+    category: ["TRAINING", "FITNESS", "ALTER"],
+    percent: 20,
+};
+
 export default function SearchForm() {
+    const [selection, setSelection] = useState<FormSelection>(initialState)
+
+    function setPosition(position: string) {
+        if (selection.position.indexOf(position) === -1) {
+            selection.position.push(position);
+        } else {
+            selection.position = selection.position.filter(pos => pos !== position);
+        }
+        setSelection({...selection, position: selection.position})
+    }
+
+    function setCategory(category: string) {
+        if (selection.category.indexOf(category) === -1) {
+            selection.category.push(category);
+        } else {
+            selection.category = selection.category.filter(cat => cat !== category);
+        }
+        setSelection({...selection, category: selection.category})
+    }
+
     return (
         <div className="grid grid-cols-2 gap-2 max-w-xs">
             <label className="form-control col-span-2">
@@ -6,14 +39,15 @@ export default function SearchForm() {
                     <span className="label-text text-xs">Position</span>
                 </div>
                 <div className="join gap-1">
-                    <button className="no-animation flex-1 btn btn-xs btn-active join-item">MD</button>
-                    <button className="no-animation flex-1 btn btn-xs btn-active join-item btn-info">RV</button>
-                    <button className="no-animation flex-1 btn btn-xs btn-active join-item btn-info">LV</button>
-                    <button className="no-animation flex-1 btn btn-xs btn-active join-item">LIB</button>
-                    <button className="no-animation flex-1 btn btn-xs btn-active join-item">LM</button>
-                    <button className="no-animation flex-1 btn btn-xs btn-active join-item">RM</button>
-                    <button className="no-animation flex-1 btn btn-xs btn-active join-item btn-info">ZM</button>
-                    <button className="no-animation flex-1 btn btn-xs btn-active join-item">ST</button>
+                    {["MD", "RV", "LV", "LIB", "LM", "RM", "ZM", "ST"].map(position => (
+                        <button key={position} className={
+                            "no-animation flex-1 btn btn-xs btn-active join-item " +
+                            (selection.position.indexOf(position) > -1 ? "btn-info" : "")}
+                                onClick={() => setPosition(position)}
+                        >
+                            {position}
+                        </button>
+                    ))}
                 </div>
             </label>
             <label className="form-control">
@@ -21,14 +55,21 @@ export default function SearchForm() {
                     <span className="label-text text-xs">Stärke</span>
                 </div>
                 <div className="flex items-center gap-2">
-                    <select className="flex-1 select select-bordered select-xs">
-                        <option>3</option>
-                        <option>4</option>
+                    <select className="flex-1 select select-bordered select-xs"
+                            value={selection.strengthFrom}
+                            onChange={e => setSelection({...selection, strengthFrom: parseInt(e.target.value)})}
+                    >
+                        {[...Array(14).keys()].slice(3).map(value => (
+                            <option key={"strengthFrom" + value}>{value}</option>
+                        ))}
                     </select>
                     <span className="text-xs">bis</span>
-                    <select className="flex-1 select select-bordered select-xs">
-                        <option>12</option>
-                        <option>13</option>
+                    <select className="flex-1 select select-bordered select-xs"
+                            value={selection.strengthTo}
+                            onChange={e => setSelection({...selection, strengthTo: parseInt(e.target.value)})}>
+                        {[...Array(14).keys()].slice(3).map(value => (
+                            <option key={"strengthTo" + value}>{value}</option>
+                        ))}
                     </select>
                 </div>
             </label>
@@ -37,15 +78,22 @@ export default function SearchForm() {
                     <span className="label-text text-xs">Alter</span>
                 </div>
                 <div className="flex items-center gap-2">
-                    <select className="flex-1 select select-bordered select-xs">
-                        <option>18</option>
-                        <option>19</option>
+                    <select className="flex-1 select select-bordered select-xs"
+                            value={selection.ageFrom}
+                            onChange={e => setSelection({...selection, ageFrom: parseInt(e.target.value)})}
+                    >
+                        {[...Array(36).keys()].slice(18).map(value => (
+                            <option key={"ageFrom" + value}>{value}</option>
+                        ))}
                     </select>
                     <span className="text-xs">bis</span>
-                    <select className="flex-1 select select-bordered select-xs">
-                        <option>33</option>
-                        <option>34</option>
-                        <option>35</option>
+                    <select className="flex-1 select select-bordered select-xs"
+                            value={selection.ageTo}
+                            onChange={e => setSelection({...selection, ageTo: parseInt(e.target.value)})}
+                    >
+                        {[...Array(36).keys()].slice(18).map(value => (
+                            <option key={"ageTo" + value}>{value}</option>
+                        ))}
                     </select>
                 </div>
             </label>
@@ -54,25 +102,36 @@ export default function SearchForm() {
                     <span className="label-text text-xs">AAW Kategorie</span>
                 </div>
                 <div className="join gap-1">
-                    <button className="no-animation flex-1 btn btn-xs btn-active join-item btn-info">TRAINING</button>
-                    <button className="no-animation flex-1 btn btn-xs btn-active join-item btn-info">FITNESS</button>
-                    <button className="no-animation flex-1 btn btn-xs btn-active join-item">EINSATZ</button>
-                    <button className="no-animation flex-1 btn btn-xs btn-active join-item btn-info">ALTER</button>
+                    {["TRAINING", "FITNESS", "EINSATZ", "ALTER"].map(category => (
+                        <button key={category}
+                                className={"no-animation flex-1 btn btn-xs btn-active join-item " +
+                                    (selection.category.indexOf(category) > -1 ? "btn-info" : "")}
+                                onClick={() => setCategory(category)}>
+                            {category}
+                        </button>
+                    ))}
                 </div>
             </label>
             <label className="form-control">
                 <div className="label">
                     <span className="label-text text-xs">Max. AAW Prozentwert ≥</span>
                 </div>
-                <select className="select select-bordered select-xs">
-                    <option>20</option>
-                    <option>21</option>
+                <select className="select select-bordered select-xs"
+                        value={selection.percent}
+                        onChange={e => setSelection({...selection, percent: parseInt(e.target.value)})}
+                >
+                    <option key="percent-inactive" value="-1"></option>
+                    {[...Array(26).keys()].slice(5).map(value => (
+                        <option key={"percent" + value}>{value}</option>
+                    ))}
                 </select>
             </label>
             <div></div>
             <div className="flex justify-center col-span-2 gap-1 mt-6">
                 <button className="btn btn-xs btn-active">Suchen</button>
-                <button className="btn btn-xs btn-active">Zurücksetzen</button>
+                <button className="btn btn-xs btn-active" type="submit"
+                        onClick={() => setSelection(initialState)}>Zurücksetzen
+                </button>
             </div>
         </div>
     )
---
+++ anstoss-transfer-market-react/model/FormSelection.ts
@@ -0,0 +1,9 @@
+export interface FormSelection {
+    ageFrom: number;
+    ageTo: number;
+    strengthFrom: number;
+    strengthTo: number;
+    position: string[];
+    category: string[];
+    percent: number;
+}
